687dd8e8b83aa1fc3182c7efc8b64b212e6089d4
AndresD <camilodelapena.estudiante@iser.edu.co>
2024-06-14 09:47:29 -0500

    Django Entorno

---
+++ MiSitio/miapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.saludo2),
+    path('consulta/',views.Consulta),
+    path('hello /<str:username>', views.hello),
+    path('Projectos/', views.projects),
+    path('task/<int:id>', views.tasks),
+    path('index/',views.Index),
+    path('about/',views.About),
+    path('Proyectos/',views.Proyectos),
+    path('Tareas/',views.tasks)
+
+]
